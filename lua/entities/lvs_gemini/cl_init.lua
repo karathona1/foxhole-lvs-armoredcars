48b1a7be23e3c4dbd1255f42da126413f6f4c0fa
TheOnly8Z <888888zombies@gmail.com>
2024-02-08 17:57:59 -0800

    remove MG glow

--- lua/entities/lvs_gemini/cl_init.lua
+++ lua/entities/lvs_gemini/cl_init.lua
@@ -2,30 +2,3 @@ include("shared.lua")
 include("cl_tankview.lua")
 include("cl_optics.lua")
 include("sh_turret.lua")
-
-function ENT:OnFrame()
-	local Heat = 0
-	if self:GetSelectedWeapon() == 1 then
-		Heat
- = self:QuickLerp( "mg_heat", self:GetNWHeat(), 10 )
-	else
-		Heat
- = self:QuickLerp( "mg_heat", 0, 0.25 )
-	end
-	local name = "gemini_mgglow_"..self:EntIndex()
-	if not self.TurretGlow2 then
-		self.TurretGlow2 = self:CreateSubMaterial( 2, name )
-
-		return
-	end
-	if self._oldGunHeat ~= Heat then
-		self._oldGunHeat
- = Heat
-
-
-		self.TurretGlow2:SetFloat("$detailblendfactor", Heat
- ^ 7 )
-
-		self:SetSubMaterial(2, "!"..name)
-	end
-end
